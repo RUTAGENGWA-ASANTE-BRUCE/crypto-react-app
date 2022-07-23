f6baf3c8ce1f9970f900f682a4ed0f42787e3b1d
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-07-23 17:50:37 +0200

    initialisiing project

---
+++ client/crypto-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import {Loader,Services,Transactions,Welcome,NavBar,Footer} from './components'
+function App() {
+
+  return (
+    <div className="">
+        Hello border-image-source: 
+    </div>
+  )
+}
+
+export default App
---
+++ client/crypto-react/src/components/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Loader from './Loader'
+import NavBar from './NavBar'
+import Services from './Services'
+import Transactions from './Transactions'
+import Welcome from './Welcome'
+import Footer from './Footer'
+// function index() {
+//   return (
+//     <div>index</div>
+//   )
+// }
+
+// export default index
